7646a0ad8ffec6c21d00bd671e5f20a127a482d9
jerome310 <dreammedia365@yahoo.com>
2024-01-06 00:15:51 -0500

    changed the setup of my liquid files within the tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: [
     "./layout/*.liquid",
     "./sections/*.liquid",
-    "./snippets/*.liquid","./templates/customers/*.liquid","./templates/*.liquid"
+    "./snippets/*.liquid",
+    "./templates/customers/*.liquid",
+    "./templates/*.liquid"
   ],
   theme: {
     extend: {},
